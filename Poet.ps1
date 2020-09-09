596fb7f05e9b797ddbe4271e69e793fb738a15d6
trackness <40263851+trackness@users.noreply.github.com>
2020-09-09 10:04:46 +0100

    Minor updates

--- Poet.ps1
+++ Poet.ps1
@@ -7,12 +7,12 @@ Clear-Host
 $Prefix = "Poet | $ProjectName |"
 $StartDir = Get-Location
 
-Write-Host $Prefix Creating new project at $StartDir\$ProjectName
+Write-Host $Prefix "Creating new project at $StartDir\$ProjectName"
 poetry new $ProjectName | Out-Null
 Set-Location $ProjectName
 
 Write-Host $Prefix "Adding project dependencies"
-$json = Get-Content -Raw -Path $PSScriptRoot/common/dependencies.json | ConvertFrom-Json
+$json = Get-Content -Raw -Path $PSScriptRoot/dependencies.json | ConvertFrom-Json
 $quiet = If ($json.quiet -eq "true") {" --quiet"} Else {""}
 $dependencies = @()
 foreach ($env in $json.env.PSObject.Properties) {
@@ -38,7 +38,6 @@ Copy-Item $PSScriptRoot/common/* | Out-Null
 
 Write-Host $Prefix "Initialising git repo"
 poetry run git init | Out-Null
-Remove-Item "README.rst" -Force
 
 Write-Host $Prefix "Making initial commit"
 poetry run git add . | Out-Null
